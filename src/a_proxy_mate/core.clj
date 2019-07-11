c7463cfbab3311c62c073be996f477c8f82404f5
kimbsy <lordkimber@gmail.com>
2019-07-11 14:56:56 +0100

    making simple proxy templates for normal cards

--- src/a_proxy_mate/core.clj
+++ src/a_proxy_mate/core.clj
@@ -1,12 +1,12 @@
 (ns a-proxy-mate.core
   (:gen-class)
   (:require [org.httpkit.client :as http]
-            [clojure.data.json :as json]
-))
+            [clojure.data.json :as json]))
 
 (def base-url "https://api.scryfall.com")
 (def fuzzy-url (str base-url "/cards/named?fuzzy="))
 (def relevant-details ["name" "mana_cost" "type_line" "oracle_text" "power" "toughness"])
+(def max-chars 32)
 
 (defn fuzzy-search
   [term]
@@ -14,34 +14,60 @@
       :body
       json/read-str))
 
+(def memoized-search (memoize fuzzy-search))
+
 (defn keywordize-key
   [[k v]]
   [(keyword k) v])
 
 (defn extract-details
   [result]
-  (-> result
-      (select-keys relevant-details)))
+  (->> (-> result
+           (select-keys relevant-details))
+       (map keywordize-key)
+       (into {})))
 
-(extract-details (fuzzy-search "lightning bolt"))
+(defn lr-align
+  [left-text right-text]
+  (let [l-count (count left-text)
+        r-count (count right-text)
+        gap-count (max 1 (- max-chars l-count r-count))]
+    (str left-text
+         (apply str (take gap-count (repeat " ")))
+         right-text)))
 
-#_'("released_at" "flavor_text" "object" "oracle_id" "related_uris" "foil" "lang" "tcgplayer_id" "collector_number" "rarity" "set_name" "oversized" "image_uris" "reserved" "rulings_uri" "games" "digital" "type_line" "uri" "multiverse_ids" "watermark" "reprint" "id" "mtgo_foil_id" "scryfall_set_uri" "prints_search_uri" "booster" "set_uri" "name" "scryfall_uri" "prices" "full_art" "oracle_text" "purchase_uris" "mtgo_id" "layout" "nonfoil" "card_back_id" "cmc" "edhrec_rank" "textless" "highres_image" "legalities" "border_color" "set_search_uri" "set_type" "set" "variation" "story_spotlight" "promo" "color_identity" "frame" "mana_cost" "colors" "artist" "illustration_id")
+(defn border
+  []
+  (apply str (take max-chars (repeat "-"))))
 
+(defn wrap-text
+  [text]
+  (->> (partition-all max-chars text)
+       (map #(apply str %))
+       (map #(str % "\n"))
+       (apply str)))
 
+(defn format-proxy
+  [{:keys [name mana_cost type_line oracle_text power toughness]}]
+  (str (border) "\n\n"
+       (wrap-text (lr-align name mana_cost)) "\n\n"
+       (wrap-text type_line) "\n\n"
+       (wrap-text oracle_text) "\n\n"
+       (when (and power toughness)
+         (str power "/" toughness "\n\n"))
+       (border) "\n"))
 
+(defn generate-proxy
+  [search-term]
+  (-> search-term
+      memoized-search
+      extract-details
+      format-proxy))
 
-
-
-
-
-
-
-
-
-
-
+;; (println (generate-proxy "lightning bolt"))
+;; (println (generate-proxy "ghalta"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (generate-proxy (first args))))
